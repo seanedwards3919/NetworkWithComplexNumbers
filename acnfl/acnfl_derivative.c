47dc3b617536051f2be2c1a87ff1ee1ba034a14d
S. Edwards <seanedwards3919@gmail.com>
2025-08-20 17:52:18 -0400

    feat:Create function object types and data management functions

---
+++ acnfl/acnfl_derivative.c
@@ -0,0 +1,58 @@
+/**
+ * @file acnfl_derivative.c
+ * Provides tools for the calcultion of numerical derivatives that integrate with acnfl_math.c.
+ **/
+
+
+/**TODO:
+    Implement simple finite differnces first
+    Implement interpolation through points
+    Implement ways to differentiate on the interpolation
+ */
+
+#include "acnfl_math.h"
+#include <stdlib.h>
+
+typedef struct acnfl_genericFunctionResult {
+    long long int resultsAmount;
+    acnfl_NumberObject* results;
+} acnfl_GenericFunctionResult;
+
+/**TODO: Is parameterCount needed? */
+typedef acnfl_GenericFunctionResult (*acnfl_GenericFunctionDefinition)(
+    long long int parameterCount, 
+    acnfl_NumberObject *parameterList) ;
+/***
+* Prints a list of function results.
+* Lists are expected to be terminated by a genericFunctionResults with resultsAmount == 0; 
+*/
+void acnfl_printListOfFunctionResults(acnfl_GenericFunctionResult *list) {
+        acnfl_GenericFunctionResult *indexPointer = list;
+    while (indexPointer->resultsAmount != 0) {
+        printf("Equation result at %p:\n", (void*)indexPointer);
+        for (int i = 0; i < indexPointer->resultsAmount; i++) {
+            printf("Result %i: ", i);
+            acnfl_printValue(indexPointer->results[i]);
+            printf("\n");
+
+        }
+
+        indexPointer++;
+    }
+    printf("Zero element founds at index %p, quitting\n", (void*)indexPointer);
+    return;
+
+}
+
+/** Frees the pointeres held by GenericFunctionResults in a list.*/
+void acnfl_freeListOfFunctionResults(acnfl_GenericFunctionResult *list, int lengthOfList) {
+        for (int i = 0; i < lengthOfList; i++ ) {
+        if (list[i].results != NULL) {
+            free(list[i].results);
+        }
+        list[i].results = NULL;
+        list[i].resultsAmount = 0;
+        
+    }
+    return;
+}
