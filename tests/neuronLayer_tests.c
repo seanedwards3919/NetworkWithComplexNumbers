a94c60f37963e398791b9e0ba9c31ba63540db6a
S. Edwards <seanedwards3919@gmail.com>
2025-10-11 21:02:43 -0400

    debug: set up setup for neuron layer testing

--- tests/neuronLayer_tests.c
+++ tests/neuronLayer_tests.c
@@ -11,22 +11,55 @@
 neuronLayer_RegularLayer neuronLayers[REGULARLAYER_NUMBER];
 #define DATASETOBJECT_NUMBER 1
 layers_DataSetObject datasets[DATASETOBJECT_NUMBER];
+#define DATASETSINGULAR_NUMBER 1
+layers_DataSetSingular datasetsSingular[DATASETSINGULAR_NUMBER];
 
-void neuronLayer_setup(void) {
+acnfl_GenericFunctionResult returnSumOfGiven(long long parameterCount, acnfl_NumberObject *parameterList) {
+    acnfl_function_preliminaryCheck(parameterCount, 0, parameterList, 1);
+        resultPointer[0] = acnfl_generateApx(0, 0);
+        for (int i = 0; i < parameterCount; i++) {
+            resultPointer[0] = acnfl_add(resultPointer[0], parameterList[i]);            
+        }
+ 
+        return toReturn;
+}
+
+
+
+void neuronLayer_setupTests(void) {
+    printf("Beginning neuronlayer tests.");
     // NETWORK ONE!!! 
     neuronLayer_initialize(neuronLayers+0);
     neuronLayer_configure(neuronLayers+0, 2, 2);
+    neuronLayer_regularLayer_zeroOut(neuronLayers[0]);
+    for (int i=0; i < (neuronLayers[0].weightMatrix_columns * neuronLayers[0].weightMatrix_rows); i++) {
+        neuronLayers[0].weightMatrix_pointer[i] = acnfl_generateApx(1, 0);
+    }
+    neuronLayers[0].activationFunction_pointer = returnSumOfGiven;
+    
+
+    // DATASET ONE !!!
+    datasets[0].dataSetType = LAYERS_DATASET_SINGULAR;
+    datasets[0].dataSet = datasetsSingular+0;
+    layers_dataSetSingular_initializeMemory(datasetsSingular+0);
+    layers_dataSetSingular_pushListOfDataPoints(
+        (acnfl_NumberObject[]) {
+            acnfl_generateApx(-1, 0), acnfl_generateApx(5, 0)
+        }, 2, datasetsSingular+0);
 
 
 }
 
-void neuronLayer_teardown(void) {
+void neuronLayer_teardownTests(void) {
     neuronLayer_destroy(neuronLayers+0);
+    free(datasetsSingular[0].dataList);
 
 }
 
 void outputErrorTests_A(void) {
     
+    printf("Beginning neuronlayer tests A.");
+    layers_feedForwardNetwork(neuronLayers+0, 1, datasets[0]);
     // Set up needed variables
 
 
---
+++ tests/tests.h
@@ -0,0 +1,19 @@
+#ifndef TESTS_H 
+#define TESTS_H 
+
+void tests_initialize_numbers();
+void test_appx_equality_inequality(void) ;
+void test_appx_addition(void);
+ void test_appx_multiplication(void) ;
+ 
+void test_appx_division(void) ;
+
+void derivative_first_round(void) ;
+ 
+void neuronLayer_setupTests(void) ;
+ 
+    void neuronLayer_teardownTests(void) ;
+
+void outputErrorTests_A(void) ;
+
+#endif
