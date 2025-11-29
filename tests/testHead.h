2f2c9d040c4591e2d13f340d54566389aae2a189
S. Edwards <seanedwards3919@gmail.com>
2025-11-28 20:52:30 -0500

    feat: lowered bar for equality

--- tests/testHead.h
+++ tests/testHead.h
@@ -11,7 +11,7 @@ CU_ErrorCode test_HeadProcess(void);
 
 
 /**Probably should've  defined these earlier.*/
-#define ACNFL_testing_equal(a, b) CU_ASSERT_EQUAL(acnfl_defaultComparison(a, b, (acnfl_defaultComparisonInformation) {.opType='a', .leeway_apx = true, .epsilon_apx = 0.0001}), 0)
+#define ACNFL_testing_equal(a, b) CU_ASSERT_EQUAL(acnfl_defaultComparison(a, b, (acnfl_defaultComparisonInformation) {.opType='a', .leeway_apx = true, .epsilon_apx = 0.001}), 0)
 #define gApx(a, b) acnfl_generateApx(a,b)
 
 
